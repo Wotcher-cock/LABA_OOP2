247363e987a323b00385770d29c3ab9fdaf25e53
Wotcher-cock <74008903+Wotcher-cock@users.noreply.github.com>
2020-11-05 22:52:44 +0500

    Version_1.1.1
    Что-то сделал и все сломалось

--- LABA_OOP2/LABA_OOP2.cpp
+++ LABA_OOP2/LABA_OOP2.cpp
@@ -4,8 +4,9 @@
 
 class Point 
 {
-public:
+protected:
     int x, y;
+public:
     Point() {
         printf("Point()\n");
         x = 0;
@@ -25,17 +26,86 @@ public:
         printf("%d, %d\n", x, y);
         printf("~Point()\n");
     }
+    void move(int dx, int dy) {
+        x = x + dx;
+        y = y + dy;
+    }
+    void reset();
 };
+
+
+void Point::reset() 
+{
+    x = 0;
+    y = 0; 
+}
+
+
+class ColoredPoint: public Point
+{
+protected:
+    int color;
+public:
+    ColoredPoint() : Point(){
+        printf("ColoredPoint()\n");
+        color = 0;
+    }
+    ColoredPoint(int x, int y, int color) : Point(x,y){
+        printf("ColoredPoint(int x, int y)\n");
+        this->color = color;
+    }
+    ColoredPoint(const ColoredPoint& p) {
+        printf("ColoredPoint(const Point &p)\n");
+        color = p.color;
+        x = p.x;
+        y = p.y;
+    }
+    ~ColoredPoint() {
+        printf("%d, %d, color=%d\n", x, y, color);
+        printf("~ColoredPoint()\n");
+    }
+    void change_color(int new_color) {
+        color = new_color;
+    }
+};
+
+class Section
+{
+protected:
+    Point *p1;
+    Point *p2;
+public:
+    Section() {
+        printf("Section()\n");
+        p1 = new Point;
+        p2 = new Point;
+    }
+   /* Section(int x, int y) {
+        printf("Section(int x, int y)\n");
+        this->x = x;
+        this->y = y;
+    }*/
+    Section(const Section& s) {
+        printf("Section(const Section &s)\n");
+        p1 = s.p1;
+        p2 = s.p2;
+    }
+    ~Section() {
+        //printf("%d, %d\n", x, y);
+        delete p1;
+        delete p2;
+        printf("~Section()\n");
+    }
+};
+
 
 int main()
 {
-    Point *p = new Point;
-    Point *p2 = new Point(10, 20);
-    Point *p3 = new Point(*p2);
+    Section *s1 = new Section;
+    Section *s2 = new Section(*s1);
 
-    delete p;
-    delete p2;
-    delete p3;
+    delete s1;
+    delete s2;
 
     return 0;
 }
