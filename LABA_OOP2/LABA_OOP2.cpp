800fe76df5910d06aff6d0587482044e17392444
Wotcher-cock <74008903+Wotcher-cock@users.noreply.github.com>
2020-11-05 21:38:45 +0500

    Version_1.1
    Сделал по видосы обьекты, диструктуры и т.д.

--- LABA_OOP2/LABA_OOP2.cpp
+++ LABA_OOP2/LABA_OOP2.cpp
@@ -1,6 +1,41 @@
 ﻿#include <iostream>
+#include <conio.h>
+
+
+class Point 
+{
+public:
+    int x, y;
+    Point() {
+        printf("Point()\n");
+        x = 0;
+        y = 0;
+    }
+    Point(int x, int y) {
+        printf("Point(int x, int y)\n");
+        this->x = x;
+        this->y = y;
+    }
+    Point(const Point &p) {
+        printf("Point(const Point &p)\n");
+        x = p.x;
+        y = p.y;
+    }
+    ~Point() {
+        printf("%d, %d\n", x, y);
+        printf("~Point()\n");
+    }
+};
 
 int main()
 {
-    std::cout << "Hello!\n";
+    Point *p = new Point;
+    Point *p2 = new Point(10, 20);
+    Point *p3 = new Point(*p2);
+
+    delete p;
+    delete p2;
+    delete p3;
+
+    return 0;
 }
